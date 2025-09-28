481201e72a599692a2f861b49baa7c22cff78f80
Sebastian Rocha <144969947+sebastianrochaleiva@users.noreply.github.com>
2025-09-27 21:26:01 -0500

    Primer Commit

---
+++ gestion-atenciones/src/main/java/com/example/gestion_atenciones/GestionAtencionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_atenciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAtencionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAtencionesApplication.class, args);
+	}
+
+}
---
+++ gestion-atenciones/src/test/java/com/example/gestion_atenciones/GestionAtencionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_atenciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAtencionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
